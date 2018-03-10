da91dfdd535c5e214866326f1138692437d4d124
Marlon <Marlon@Marlon-PC>
2018-03-10 23:04:46 +0100

    CommandList done

--- src/main/java/de/simcom/App.java
+++ src/main/java/de/simcom/App.java
@@ -1,20 +1,27 @@
 package de.simcom;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 
+import de.simcom.games.arma.commandMachine.impl.CommandList;
 import freemarker.core.ParseException;
 import freemarker.template.MalformedTemplateNameException;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateNotFoundException;
 
 public class App {
-	public String test = "test";
 
 	public static void main(String[] args) throws SQLException, TemplateNotFoundException,
 			MalformedTemplateNameException, ParseException, IOException, TemplateException {
 
 		// Application.launch(View.class, args);
+		CommandList cl = new CommandList();
+		try {
+			cl.getCommands();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
--- src/main/java/de/simcom/games/arma/commandMachine/impl/CommandList.java
+++ src/main/java/de/simcom/games/arma/commandMachine/impl/CommandList.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
 import org.jsoup.select.Elements;
 
 import de.simcom.games.arma.commandMachine.CommandMachine;
@@ -35,17 +36,31 @@ public class CommandList implements CommandMachine {
 					returner += e.wholeText() + "\n";
 				}
 			}
-			;
 		}
-		;
-		return returner.replaceAll(" ", "_").replaceAll("_%", "_%25");
 
+		return returner.replaceAll(" ", "_").replaceAll("_%", "_%25");
+	}
+
+	public void getCommandsOld() throws IOException {
+		for (String command : getCommandList().split("\n")) {
+			String url = "https://community.bistudio.com/wiki/" + command;
+			Document doc = Jsoup.connect(url).get();
+			writer = new BufferedWriter(new FileWriter(new File("commandList.txt"), true));
+			Element div = doc.getElementsByClass("_description cmd").first();
+			Elements divChildren = div.children();
+
+			Elements detachedDivChildren = new Elements();
+			for (Element elem : divChildren) {
+				Element detachedChild = new Element(Tag.valueOf(elem.tagName()), elem.baseUri(),
+						elem.attributes().clone());
+				detachedDivChildren.add(detachedChild);
+			}
+		}
 	}
 
 	public void getCommands() throws IOException, URISyntaxException {
-		List<String> list = new ArrayList<String>();
-
-		for (String command : getCommandList().split("\n")) {
+		String commandList = getCommandList();
+		for (String command : commandList.split("\n")) {
 			String url = "https://community.bistudio.com/wiki/" + command;
 			Document doc = Jsoup.connect(url).get();
 			writer = new BufferedWriter(new FileWriter(new File("commandList.txt"), true));
@@ -53,15 +68,24 @@ public class CommandList implements CommandMachine {
 				if (e.getElementsContainingText("Syntax:").size() > 0) {
 					String syntax = (e.getElementsContainingText("Syntax:").get(0).wholeText().replaceAll("\n",
 							"")) == null ? "" : e.getElementsContainingText("Syntax:").get(0).wholeText();
-					//
-					if (syntax.contains("\n")) {
-						syntax.replaceAll("\n", "");
+
+					if (!syntax.contains("Posted") && !syntax.contains("Alternative Syntax")) {
+						//
+						if (syntax.contains("\n")) {
+							syntax = syntax.split("\n")[2];
+						}
+						// syntax = syntax.split("Syntax:")[1];
+						if (syntax.contains("Parameters")) {
+							syntax = syntax.split("Parameters:")[0] + "\n";
+						}
+						if (syntax.contains("Return Value")) {
+							syntax = syntax.split("Return Value")[0];
+						}
+						syntax = syntax.replaceAll("Syntax:", "").trim().replaceAll("\\u00a0.+", "")
+						.replaceAll("  .+", "") + "\n";
+						
+						writer.write(syntax);
 					}
-					// syntax = syntax.split("Syntax:")[1];
-					if (syntax.contains("Parameters")) {
-						syntax = syntax.split("Parameters:")[0] + "\n";
-					}
-					writer.write(syntax);
 
 				}
 
@@ -70,14 +94,5 @@ public class CommandList implements CommandMachine {
 			writer.close();
 		}
 		;
-
-		/*
-		 * for (String t : getCommandList().split("\n")) { String url =
-		 * "https://community.bistudio.com/wiki/" + t; Document doc =
-		 * Jsoup.connect(url).get(); Elements elements =
-		 * doc.getElementsByIndexEquals(0); list.add(elements.get(0).wholeText());
-		 * System.out.println(url); } for (String t : list) { System.out.println(t); }
-		 */
 	}
-
 }
